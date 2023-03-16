26e1bee899fb30f654bc2ee39ee19a053cf3baa9
usmanlubis <usmanlubis11@gmail.com>
2023-03-16 10:39:40 +0700

    bring back NotesApp component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-// import NotesApp from './component/NotesApp';
+import NotesApp from './component/NotesApp';
 
 function App() {
   return (
-    // <NotesApp />
-    <div>Hello</div>
+    <NotesApp />
   );
 }
 
