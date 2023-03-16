6a5b575c3d58bd72d90eab02d028108de89cfe4d
usmanlubis <usmanlubis11@gmail.com>
2023-03-16 10:36:05 +0700

    test the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import NotesApp from './component/NotesApp';
+// import NotesApp from './component/NotesApp';
 
 function App() {
   return (
-    <NotesApp />
+    // <NotesApp />
+    <div>Hello</div>
   );
 }
 
